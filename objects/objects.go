24f7d18224cf77a162ca8737ea5cd478ec84df1e
Singwa Cheng <developer.chengs@gmail.com>
2018-09-20 18:40:15 -0400

    function now initializes object

--- objects/objects.go
+++ objects/objects.go
@@ -1,59 +1,64 @@
 package objects
 
 import (
-    "encoding/csv"
-    "fmt"
+	"bufio"
+	"encoding/csv"
+	"fmt"
 	"os"
-    "strconv"
 )
 
-type Station struct{
-	Route string
-	StationName string
-	TimeTable []string 
+type Station struct {
+	Route       string   `json:_id bson:_id`
+	StationName string   `json:stationame bson:stationname`
+	TimeTable   []string `json:timetable bson:timetable`
 }
 
-type Bus struct{
-	Route string
-	StationList []Station
+type Bus struct {
+	Route       string    `json:route bson:route`
+	StationList []Station `json:stationlist bson:stationlist`
 }
 
 func ParseCSV() {
-    //create reader to read filename
+	//create reader to read filename
 
-    // Open CSV file
-    f, err := os.Open("Book1.csv")
-    if err != nil {
-        panic(err)
-    }
-    defer f.Close()
+	f, err := os.Open("Book1.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	//reading route
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter Route: ")
+	route, _ := reader.ReadString('\n')
 
-    // Read File into a Variable
-    lines, err := csv.NewReader(f).ReadAll()
-    if err != nil {
-        panic(err)
-    }
+	// Read File into a Variable
+	lines, err := csv.NewReader(f).ReadAll()
+	if err != nil {
+		panic(err)
+	}
 
-	// get the number of rows 
+	// get the number of rows
 	rowNum := len(lines)
 	// get the number of station (first row)
 	stationNum := len(lines[0])
-    
-    //loop through the file by column
-	for currentColumn:=0; currentColumn<stationNum; currentColumn++{
-        stationName := ""
-        stationTT := []string{}
-		for currentRow:=0;currentRow < rowNum; currentRow++{
-            if currentRow == 0 {
-                stationName = lines[currentRow][currentColumn]
-                fmt.Println("station name is: " + stationName)
-                currentRow++
-            }
-            fmt.Println("appending time: " + lines[currentRow][currentColumn])
-            stationTT = append(stationTT, lines[currentRow][currentColumn])
-            fmt.Println("size of array rn: " + strconv.Itoa(len(stationTT)))
-                
-            }
-        }
-    }
-    
+	// list of station
+	stationList := []Station{}
+	//loop through the file by column
+	for currentColumn := 0; currentColumn < stationNum; currentColumn++ {
+		stationName := ""
+		stationTT := []string{}
+		for currentRow := 0; currentRow < rowNum; currentRow++ {
+			if currentRow == 0 {
+				stationName = lines[currentRow][currentColumn]
+				currentRow++
+			}
+			stationTT = append(stationTT, lines[currentRow][currentColumn])
+		}
+		newStation := Station{Route: route, StationName: stationName, TimeTable: stationTT}
+		stationList = append(stationList, newStation)
+
+	}
+	//create new bus and push to database
+	newBus := Bus{Route: route, StationList: stationList}
+	//missing database functions for now
+}
