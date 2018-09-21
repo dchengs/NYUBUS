55e18100937e5c8d866fa135162b9fce222bc4c5
Singwa Cheng <developer.chengs@gmail.com>
2018-09-20 23:21:25 -0400

    implemented find function

--- main/main.go
+++ main/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"nyubus/common"
+	"nyubus/mongodb"
 )
 
 func main() {
 	common.ParseCSV()
+	mongodb.Find("nyubus", "route", "Z\n")
 }
--- mongodb/find.go
+++ mongodb/find.go
@@ -1,5 +1,26 @@
 package mongodb
 
-func Find() {
+import (
+	dt "nyubus/datatypes"
 
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+//Find takes database, collection, and route to search for associated bus object and return it if found
+func Find(database, collection, route string) (dt.Bus, error) {
+
+	session, err := mgo.Dial("127.0.0.1")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+	c := session.DB(database).C(collection)
+	//result is of type bus
+	result := dt.Bus{}
+	err = c.Find(bson.M{"_id": route}).One(&result)
+	if err != nil {
+		return result, err
+	}
+	return result, nil
 }
